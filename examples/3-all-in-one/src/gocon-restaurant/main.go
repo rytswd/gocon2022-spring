a53febbb3c1052bf4dede00e388075ca53e775e2
Ryota <rytswd@gmail.com>
2022-04-18 02:13:59 +0100

    Add gocon-restaurant

---
+++ examples/3-all-in-one/src/gocon-restaurant/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"go.rytswd/gocon-main-demo/examples/3-all-in-one/src/uk-farm/beef"
+)
+
+func main() {
+	fmt.Printf("Cooking")
+	time.Sleep(time.Second)
+	fmt.Printf(".")
+	time.Sleep(time.Second)
+	fmt.Printf(".")
+	time.Sleep(time.Second)
+	fmt.Printf(".\n")
+
+	m := beef.BeefForSteak()
+
+	fmt.Printf("Here goes your Steak!\n\n-- Ingredients were:\n\t%s", m)
+}
